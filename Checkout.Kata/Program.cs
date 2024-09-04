a9358ce5a580768e1813ada0c2faefeb52d2f934
rinibhasin <rinibhasin17july@gmail.com>
2024-09-04 15:29:20 +0100

    Add driver code

--- Checkout.Kata/Program.cs
+++ Checkout.Kata/Program.cs
@@ -1,7 +1,32 @@
-﻿public class Program
+﻿using Checkout.Kata.Services;
+using Checkout.Kata.Services.Interfaces;
+using Checkout.Kata.Services;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-  
+        IDictionary<string, IPricingRule> _rules;
+
+        
+        _rules = new Dictionary<string, IPricingRule>()
+        {
+            { "A", new SpecialPricingRule(3, 130, 50) },
+            { "B", new SpecialPricingRule(2, 45, 30) },
+            { "C", new UnitPricingRule(20) },
+            { "D", new UnitPricingRule(15) },
+          
+        };
+
+        Checkout.Kata.Services.Checkout checkout = new Checkout.Kata.Services.Checkout(_rules);
+        
+        
+        checkout.Scan("A");
+        checkout.Scan("A");
+        checkout.Scan("A");
+
+        checkout.GetTotalPrice();
+
+        Console.ReadLine();
     }
 }
